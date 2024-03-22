6364e07ebe374b431256beb12c40f8c5fb980b10
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-03-22 11:35:49 +0530

    Add files via upload

---
+++ fifth-batch/js.js
@@ -0,0 +1,104 @@
+// Sample data (you can replace this with your actual data)
+let i = 1;
+const data = [
+ // add data here
+[i++,"AKASHYADAV","53533707","./fifth batch/AKASHYADAV_53533707/1.bmp","./fifth batch/AKASHYADAV_53533707/2.bmp","./fifth batch/AKASHYADAV_53533707/3.bmp","./fifth batch/AKASHYADAV_53533707/4.bmp","./fifth batch/AKASHYADAV_53533707/5.bmp"],
+[i++,"amityadav","9184941","./fifth batch/amityadav_9184941/1.bmp","./fifth batch/amityadav_9184941/2.bmp","./fifth batch/amityadav_9184941/3.bmp","./fifth batch/amityadav_9184941/4.bmp","./fifth batch/amityadav_9184941/5.bmp"],
+[i++,"ANURDHAMISHRA","26158854","./fifth batch/ANURDHAMISHRA_26158854/1.bmp","./fifth batch/ANURDHAMISHRA_26158854/2.bmp","./fifth batch/ANURDHAMISHRA_26158854/3.bmp","./fifth batch/ANURDHAMISHRA_26158854/4.bmp","./fifth batch/ANURDHAMISHRA_26158854/5.bmp"],
+[i++,"BABY","86560904","./fifth batch/BABY_86560904/1.bmp","./fifth batch/BABY_86560904/2.bmp","./fifth batch/BABY_86560904/3.bmp","./fifth batch/BABY_86560904/4.bmp","./fifth batch/BABY_86560904/5.bmp"],
+[i++,"DALIYADAV","41473672","./fifth batch/DALIYADAV_41473672/1.bmp","./fifth batch/DALIYADAV_41473672/2.bmp","./fifth batch/DALIYADAV_41473672/3.bmp","./fifth batch/DALIYADAV_41473672/4.bmp","./fifth batch/DALIYADAV_41473672/5.bmp"],
+[i++,"JYOTITIWARI","9856341","./fifth batch/JYOTITIWARI_9856341/1.bmp","./fifth batch/JYOTITIWARI_9856341/2.bmp","./fifth batch/JYOTITIWARI_9856341/3.bmp","./fifth batch/JYOTITIWARI_9856341/4.bmp","./fifth batch/JYOTITIWARI_9856341/5.bmp"],
+[i++,"RENUYDV","13351438","./fifth batch/RENUYDV_13351438/1.bmp","./fifth batch/RENUYDV_13351438/2.bmp","./fifth batch/RENUYDV_13351438/3.bmp","./fifth batch/RENUYDV_13351438/4.bmp","./fifth batch/RENUYDV_13351438/5.bmp"],
+[i++,"SAHEEN","65211250","./fifth batch/SAHEEN_65211250/1.bmp","./fifth batch/SAHEEN_65211250/2.bmp","./fifth batch/SAHEEN_65211250/3.bmp","./fifth batch/SAHEEN_65211250/4.bmp","./fifth batch/SAHEEN_65211250/5.bmp"],
+[i++,"SANDHYARJ","17429771","./fifth batch/SANDHYARJ_17429771/1.bmp","./fifth batch/SANDHYARJ_17429771/2.bmp","./fifth batch/SANDHYARJ_17429771/3.bmp","./fifth batch/SANDHYARJ_17429771/4.bmp","./fifth batch/SANDHYARJ_17429771/5.bmp"],
+[i++,"sangeeta","90513247","./fifth batch/sangeeta_90513247/1.bmp","./fifth batch/sangeeta_90513247/2.bmp","./fifth batch/sangeeta_90513247/3.bmp","./fifth batch/sangeeta_90513247/4.bmp","./fifth batch/sangeeta_90513247/5.bmp"],
+[i++,"satish","33832489","./fifth batch/satish_33832489/1.bmp","./fifth batch/satish_33832489/2.bmp","./fifth batch/satish_33832489/3.bmp","./fifth batch/satish_33832489/4.bmp","./fifth batch/satish_33832489/5.bmp"],
+[i++,"SAURBHKUMAR","54914732","./fifth batch/SAURBHKUMAR_54914732/1.bmp","./fifth batch/SAURBHKUMAR_54914732/2.bmp","./fifth batch/SAURBHKUMAR_54914732/3.bmp","./fifth batch/SAURBHKUMAR_54914732/4.bmp","./fifth batch/SAURBHKUMAR_54914732/5.bmp"],
+[i++,"sejaljaiswal","96175939","./fifth batch/sejaljaiswal_96175939/1.bmp","./fifth batch/sejaljaiswal_96175939/2.bmp","./fifth batch/sejaljaiswal_96175939/3.bmp","./fifth batch/sejaljaiswal_96175939/4.bmp","./fifth batch/sejaljaiswal_96175939/5.bmp"],
+[i++,"SHALUYADAV","58475342","./fifth batch/SHALUYADAV_58475342/1.bmp","./fifth batch/SHALUYADAV_58475342/2.bmp","./fifth batch/SHALUYADAV_58475342/3.bmp","./fifth batch/SHALUYADAV_58475342/4.bmp","./fifth batch/SHALUYADAV_58475342/5.bmp"],
+[i++,"SHIVANGEE","12345678","./fifth batch/SHIVANGEE_12345678/1.bmp","./fifth batch/SHIVANGEE_12345678/2.bmp","./fifth batch/SHIVANGEE_12345678/3.bmp","./fifth batch/SHIVANGEE_12345678/4.bmp","./fifth batch/SHIVANGEE_12345678/5.bmp"],
+[i++,"SHIVANGISANGEET","83247155","./fifth batch/SHIVANGISANGEET_83247155/1.bmp","./fifth batch/SHIVANGISANGEET_83247155/2.bmp","./fifth batch/SHIVANGISANGEET_83247155/3.bmp","./fifth batch/SHIVANGISANGEET_83247155/4.bmp","./fifth batch/SHIVANGISANGEET_83247155/5.bmp"],
+[i++,"shubhamy","61324215","./fifth batch/shubhamy_61324215/1.bmp","./fifth batch/shubhamy_61324215/2.bmp","./fifth batch/shubhamy_61324215/3.bmp","./fifth batch/shubhamy_61324215/4.bmp","./fifth batch/shubhamy_61324215/5.bmp"],
+[i++,"sonamjaiswal","97450465","./fifth batch/sonamjaiswal_97450465/1.bmp","./fifth batch/sonamjaiswal_97450465/2.bmp","./fifth batch/sonamjaiswal_97450465/3.bmp","./fifth batch/sonamjaiswal_97450465/4.bmp","./fifth batch/sonamjaiswal_97450465/5.bmp"],
+[i++,"suraj","68976872","./fifth batch/suraj_68976872/1.bmp","./fifth batch/suraj_68976872/2.bmp","./fifth batch/suraj_68976872/3.bmp","./fifth batch/suraj_68976872/4.bmp","./fifth batch/suraj_68976872/5.bmp"],
+[i++,"ujjalahupta","86043235","./fifth batch/ujjalahupta_86043235/1.bmp","./fifth batch/ujjalahupta_86043235/2.bmp","./fifth batch/ujjalahupta_86043235/3.bmp","./fifth batch/ujjalahupta_86043235/4.bmp","./fifth batch/ujjalahupta_86043235/5.bmp"],
+[i++,"VANDANA","93131798","./fifth batch/VANDANA_93131798/1.bmp","./fifth batch/VANDANA_93131798/2.bmp","./fifth batch/VANDANA_93131798/3.bmp","./fifth batch/VANDANA_93131798/4.bmp","./fifth batch/VANDANA_93131798/5.bmp"],
+[i++,"VISHALRAJBHAR","6801213","./fifth batch/VISHALRAJBHAR_6801213/1.bmp","./fifth batch/VISHALRAJBHAR_6801213/2.bmp","./fifth batch/VISHALRAJBHAR_6801213/3.bmp","./fifth batch/VISHALRAJBHAR_6801213/4.bmp","./fifth batch/VISHALRAJBHAR_6801213/5.bmp"],
+
+         // 1-30 are done....
+// adding more rows..
+
+];
+
+
+let currentSortColumn = -1; // Initially, no column is sorted
+let isAscending = true; // Initially, sorting is in ascending order
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderTable(data);
+    
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const filteredData = data.filter(row => row.some(cell => cell.toString().toLowerCase().includes(searchTerm)));
+        renderTable(filteredData);
+    });
+});
+
+function renderTable(data) {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+
+            if (index >= 3 && index <= 7) {
+                const img = document.createElement('img');
+                img.src = cell;
+                img.alt = `Finger ${index - 1}`;
+                img.style.width = '100px'; // Set width to auto for actual size
+                img.style.height = '100px'; // Set height to auto for actual size
+                img.style.transform = 'scaleX(-1)'; // Mirror horizontally
+                img.style.filter = 'invert(100%)'; // Color invert
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+
+    updateTotalCount(data.length);
+}
+
+function updateTotalCount(count) {
+    const totalCountElement = document.getElementById('totalCount');
+    totalCountElement.textContent = `Total Count: ${count}`;
+}
+
+function sortTable(columnIndex) {
+    if (currentSortColumn === columnIndex) {
+        // If the same column is clicked again, reverse the sorting order
+        isAscending = !isAscending;
+    } else {
+        // If a new column is clicked, set the sorting order to ascending
+        isAscending = true;
+        currentSortColumn = columnIndex;
+    }
+
+    data.sort((a, b) => {
+        const valueA = a[currentSortColumn];
+        const valueB = b[currentSortColumn];
+
+        // Customize the comparison logic based on the data type (string, number, etc.)
+        return isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+
+    renderTable(data);
+}
