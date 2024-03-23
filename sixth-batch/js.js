b80862e55390e873659d213b82fada49f691761a
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-03-23 12:04:39 +0530

    Add files via upload

---
+++ sixth-batch/js.js
@@ -0,0 +1,102 @@
+// Sample data (you can replace this with your actual data)
+let i = 1;
+const data = [
+ // add data here
+
+  [i++, "Aarij", "60211022", "./sixth batch/Aarij_60211022/1.bmp", "./sixth batch/Aarij_60211022/2.bmp", "./sixth batch/Aarij_60211022/3.bmp", "./sixth batch/Aarij_60211022/4.bmp", "./sixth batch/Aarij_60211022/5.bmp"],
+  [i++, "Ankit Yadav", "170886", "./sixth batch/Ankit Yadav_170886/1.bmp", "./sixth batch/Ankit Yadav_170886/2.bmp", "./sixth batch/Ankit Yadav_170886/3.bmp", "./sixth batch/Ankit Yadav_170886/4.bmp", "./sixth batch/Ankit Yadav_170886/5.bmp"],
+  [i++, "Kajal", "68980581", "./sixth batch/Kajal_68980581/1.bmp", "./sixth batch/Kajal_68980581/2.bmp", "./sixth batch/Kajal_68980581/3.bmp", "./sixth batch/Kajal_68980581/4.bmp", "./sixth batch/Kajal_68980581/5.bmp"],
+  [i++, "Kaynat Jahan", "19593226", "./sixth batch/Kaynat Jahan_19593226/1.bmp", "./sixth batch/Kaynat Jahan_19593226/2.bmp", "./sixth batch/Kaynat Jahan_19593226/3.bmp", "./sixth batch/Kaynat Jahan_19593226/4.bmp", "./sixth batch/Kaynat Jahan_19593226/5.bmp"],
+  [i++, "Km. Ragani Yadav", "57088922", "./sixth batch/Km. Ragani Yadav_57088922/1.bmp", "./sixth batch/Km. Ragani Yadav_57088922/2.bmp", "./sixth batch/Km. Ragani Yadav_57088922/3.bmp", "./sixth batch/Km. Ragani Yadav_57088922/4.bmp", "./sixth batch/Km. Ragani Yadav_57088922/5.bmp"],
+  [i++, "Krishan Yadav", "8557043", "./sixth batch/Krishan Yadav_8557043/1.bmp", "./sixth batch/Krishan Yadav_8557043/2.bmp", "./sixth batch/Krishan Yadav_8557043/3.bmp", "./sixth batch/Krishan Yadav_8557043/4.bmp", "./sixth batch/Krishan Yadav_8557043/5.bmp"],
+  [i++, "Manisha", "26370792", "./sixth batch/Manisha_26370792/1.bmp", "./sixth batch/Manisha_26370792/2.bmp", "./sixth batch/Manisha_26370792/3.bmp", "./sixth batch/Manisha_26370792/4.bmp", "./sixth batch/Manisha_26370792/5.bmp"],
+  [i++, "NEHAvsk", "", "./sixth batch/NEHAvsk/1.bmp", "./sixth batch/NEHAvsk/2.bmp", "./sixth batch/NEHAvsk/3.bmp", "./sixth batch/NEHAvsk/4.bmp", "./sixth batch/NEHAvsk/5.bmp"],
+  [i++, "Nisha Yadav", "28438268", "./sixth batch/Nisha Yadav_28438268/1.bmp", "./sixth batch/Nisha Yadav_28438268/2.bmp", "./sixth batch/Nisha Yadav_28438268/3.bmp", "./sixth batch/Nisha Yadav_28438268/4.bmp", "./sixth batch/Nisha Yadav_28438268/5.bmp"],
+  [i++, "Pawan Rai", "25219417", "./sixth batch/Pawan Rai_25219417/1.bmp", "./sixth batch/Pawan Rai_25219417/2.bmp", "./sixth batch/Pawan Rai_25219417/3.bmp", "./sixth batch/Pawan Rai_25219417/4.bmp", "./sixth batch/Pawan Rai_25219417/5.bmp"],
+  [i++, "Poonam Yadav", "46663769", "./sixth batch/Poonam Yadav_46663769/1.bmp", "./sixth batch/Poonam Yadav_46663769/2.bmp", "./sixth batch/Poonam Yadav_46663769/3.bmp", "./sixth batch/Poonam Yadav_46663769/4.bmp", "./sixth batch/Poonam Yadav_46663769/5.bmp"],
+  [i++, "Prashant Kumar", "27748044", "./sixth batch/Prashant Kumar_27748044/1.bmp", "./sixth batch/Prashant Kumar_27748044/2.bmp", "./sixth batch/Prashant Kumar_27748044/3.bmp", "./sixth batch/Prashant Kumar_27748044/4.bmp", "./sixth batch/Prashant Kumar_27748044/5.bmp"],
+  [i++, "Ravikant Yadav", "13267542", "./sixth batch/Ravikant Yadav_13267542/1.bmp", "./sixth batch/Ravikant Yadav_13267542/2.bmp", "./sixth batch/Ravikant Yadav_13267542/3.bmp", "./sixth batch/Ravikant Yadav_13267542/4.bmp", "./sixth batch/Ravikant Yadav_13267542/5.bmp"],
+  [i++, "Reetu Yadav", "69068448", "./sixth batch/Reetu Yadav_69068448/1.bmp", "./sixth batch/Reetu Yadav_69068448/2.bmp", "./sixth batch/Reetu Yadav_69068448/3.bmp", "./sixth batch/Reetu Yadav_69068448/4.bmp", "./sixth batch/Reetu Yadav_69068448/5.bmp"],
+  [i++, "Sachin Rajbhar", "86136016", "./sixth batch/Sachin Rajbhar_86136016/1.bmp", "./sixth batch/Sachin Rajbhar_86136016/2.bmp", "./sixth batch/Sachin Rajbhar_86136016/3.bmp", "./sixth batch/Sachin Rajbhar_86136016/4.bmp", "./sixth batch/Sachin Rajbhar_86136016/5.bmp"],
+  [i++, "Samreen Fatma", "80507987", "./sixth batch/Samreen Fatma_80507987/1.bmp", "./sixth batch/Samreen Fatma_80507987/2.bmp", "./sixth batch/Samreen Fatma_80507987/3.bmp", "./sixth batch/Samreen Fatma_80507987/4.bmp", "./sixth batch/Samreen Fatma_80507987/5.bmp"],
+  [i++, "Saniya Bano", "69282547", "./sixth batch/Saniya Bano_69282547/1.bmp", "./sixth batch/Saniya Bano_69282547/2.bmp", "./sixth batch/Saniya Bano_69282547/3.bmp", "./sixth batch/Saniya Bano_69282547/4.bmp", "./sixth batch/Saniya Bano_69282547/5.bmp"],
+  [i++, "Satyendra Yadav", "8275771", "./sixth batch/Satyendra Yadav_8275771/1.bmp", "./sixth batch/Satyendra Yadav_8275771/2.bmp", "./sixth batch/Satyendra Yadav_8275771/3.bmp", "./sixth batch/Satyendra Yadav_8275771/4.bmp", "./sixth batch/Satyendra Yadav_8275771/5.bmp"],
+  [i++, "Vipin Yadav", "46216680", "./sixth batch/Vipin Yadav_46216680/1.bmp", "./sixth batch/Vipin Yadav_46216680/2.bmp", "./sixth batch/Vipin Yadav_46216680/3.bmp", "./sixth batch/Vipin Yadav_46216680/4.bmp", "./sixth batch/Vipin Yadav_46216680/5.bmp"],
+
+         // 1-30 are done....
+// adding more rows..
+
+];
+
+
+let currentSortColumn = -1; // Initially, no column is sorted
+let isAscending = true; // Initially, sorting is in ascending order
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderTable(data);
+    
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const filteredData = data.filter(row => row.some(cell => cell.toString().toLowerCase().includes(searchTerm)));
+        renderTable(filteredData);
+    });
+});
+
+function renderTable(data) {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+
+            if (index >= 3 && index <= 7) {
+                const img = document.createElement('img');
+                img.src = cell;
+                img.alt = `Finger ${index - 1}`;
+                img.style.width = '100px'; // Set width to auto for actual size
+                img.style.height = '100px'; // Set height to auto for actual size
+                img.style.transform = 'scaleX(-1)'; // Mirror horizontally
+                img.style.filter = 'invert(100%)'; // Color invert
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+
+    updateTotalCount(data.length);
+}
+
+function updateTotalCount(count) {
+    const totalCountElement = document.getElementById('totalCount');
+    totalCountElement.textContent = `Total Count: ${count}`;
+}
+
+function sortTable(columnIndex) {
+    if (currentSortColumn === columnIndex) {
+        // If the same column is clicked again, reverse the sorting order
+        isAscending = !isAscending;
+    } else {
+        // If a new column is clicked, set the sorting order to ascending
+        isAscending = true;
+        currentSortColumn = columnIndex;
+    }
+
+    data.sort((a, b) => {
+        const valueA = a[currentSortColumn];
+        const valueB = b[currentSortColumn];
+
+        // Customize the comparison logic based on the data type (string, number, etc.)
+        return isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+
+    renderTable(data);
+}
