12f6ace57c43f24430fc40c47fe137c61e6dfd32
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-04-13 14:57:25 +0530

    Update js.js

--- fifth-batch/js.js
+++ fifth-batch/js.js
@@ -31,6 +31,7 @@ const data = [
   [i++, "Raj Kumar Yadav", "39534580", "./fifth batch/Raj Kumar Yadav_39534580/1.bmp", "./fifth batch/Raj Kumar Yadav_39534580/2.bmp", "./fifth batch/Raj Kumar Yadav_39534580/3.bmp", "./fifth batch/Raj Kumar Yadav_39534580/4.bmp", "./fifth batch/Raj Kumar Yadav_39534580/5.bmp"],
   [i++, "Vishal Yadav", "08460142", "./fifth batch/Vishal Yadav_08460142/1.bmp", "./fifth batch/Vishal Yadav_08460142/2.bmp", "./fifth batch/Vishal Yadav_08460142/3.bmp", "./fifth batch/Vishal Yadav_08460142/4.bmp", "./fifth batch/Vishal Yadav_08460142/5.bmp"]
   [i++, "ARUN KUMAR", "59276405", "./fifth batch/ARUN KUMAR_59276405/1.bmp", "./fifth batch/ARUN KUMAR_59276405/2.bmp", "./fifth batch/ARUN KUMAR_59276405/3.bmp", "./fifth batch/ARUN KUMAR_59276405/4.bmp", "./fifth batch/ARUN KUMAR_59276405/5.bmp"]
+  [i++, "Shibu", "38942150", "./fifth batch/Shibu_38942150/1.bmp", "./fifth batch/Shibu_38942150/2.bmp", "./fifth batch/Shibu_38942150/3.bmp", "./fifth batch/Shibu_38942150/4.bmp", "./fifth batch/Shibu_38942150/5.bmp"],
 
          // 1-30 are done....
 // adding more rows..
