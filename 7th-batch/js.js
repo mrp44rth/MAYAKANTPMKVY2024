6e55543c1bf1394d4005808b6a4df485bd409cdb
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-04-24 19:48:54 +0530

    Update js.js

--- 7th-batch/js.js
+++ 7th-batch/js.js
@@ -27,6 +27,7 @@ const data = [
   [i++, "RESHAMAYADAV", "26859745", "./7th batch/RESHAMAYADAV_26859745/1.bmp", "./7th batch/RESHAMAYADAV_26859745/2.bmp", "./7th batch/RESHAMAYADAV_26859745/3.bmp", "./7th batch/RESHAMAYADAV_26859745/4.bmp", "./7th batch/RESHAMAYADAV_26859745/5.bmp"],
   [i++, "swetayadav", "07042330", "./7th batch/swetayadav_07042330/1.bmp", "./7th batch/swetayadav_07042330/2.bmp", "./7th batch/swetayadav_07042330/3.bmp", "./7th batch/swetayadav_07042330/4.bmp", "./7th batch/swetayadav_07042330/5.bmp"],
   [i++, "Suman", "67739733", "./7th batch/Suman_67739733/1.bmp", "./7th batch/Suman_67739733/2.bmp", "./7th batch/Suman_67739733/3.bmp", "./7th batch/Suman_67739733/4.bmp", "./7th batch/Suman_67739733/5.bmp"],
+[i++, "Sonam", "70137844", "./7th batch/Sonam_70137844/1.bmp", "./7th batch/Sonam_70137844/2.bmp", "./7th batch/Sonam_70137844/3.bmp", "./7th batch/Sonam_70137844/4.bmp", "./7th batch/Sonam_70137844/5.bmp"],
 
          // 1-30 are done....
 // adding more rows..
